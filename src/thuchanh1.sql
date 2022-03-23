f6b4de8cb0566284c6f7cfb625db52f0c3de66bf
ducdaudshp <ducdaudshp@gmail.com>
2022-03-23 14:19:09 +0700

    proceduce

--- src/thuchanh1.sql
+++ src/thuchanh1.sql
@@ -9,3 +9,28 @@ ALTER TABLE customers ADD INDEX idx_full_name(contactFirstName, contactLastName)
 EXPLAIN SELECT * FROM customers WHERE contactFirstName = 'Jean' or contactFirstName = 'King';
 
 ALTER TABLE customers DROP INDEX idx_full_name;
+
+DELIMITER //
+
+CREATE PROCEDURE findAllCustomers()
+
+BEGIN
+
+    SELECT * FROM customers;
+
+END //
+
+DELIMITER ;
+call findAllCustomers();
+
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS `findAllCustomers`//
+
+CREATE PROCEDURE findAllCustomers()
+
+BEGIN
+
+    SELECT * FROM customers where customerNumber = 175;
+
+END //
